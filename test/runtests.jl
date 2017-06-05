2e246a4552cf7bb68eb634e79a27de72e11f625f
JPD <vepiteski@gmail.com>
2017-06-05 17:55:00 -0400

    Adjusted the tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,7 +18,7 @@ include("steepestS.jl")
 @printf("Problem  Dim  Optim f  Grad norm  f evals g evals Hv evals  iters  outcome         time\n")
 
 
-s = TStopping(nlp, max_iter = 1) # to trigger compilation of steepest
+s = TStopping(max_iter = 1) # to trigger compilation of steepest
 (x, f, gNorm, iter, optimal, tired, status, Stime) = steepest(nlp, s=s, verbose=false)
 @printf("%-5s  %3d  %9.2e  %7.1e  %5d  %5d  %6d   %6d  %-20s  %7.3e\n",
         nlp.meta.name, nlp.meta.nvar, f, gNorm,
@@ -28,7 +28,7 @@ s = TStopping(nlp, max_iter = 1) # to trigger compilation of steepest
 @test status == :UserLimit
 
 reset!(nlp)
-s = TStopping(nlp, rtol=0.0, max_eval = 500000, max_iter = 100000, max_time = 1.0)
+s = TStopping(rtol=0.0, max_eval = 500000, max_iter = 100000, max_time = 1.0)
 (x, f, gNorm, iter, optimal, tired, status, Stime) = steepest(nlp, s=s, verbose=false)
 @printf("%-5s  %3d  %9.2e  %7.1e  %5d  %5d  %6d   %6d  %-20s  %7.3e\n",
         nlp.meta.name, nlp.meta.nvar, f, gNorm,
@@ -39,7 +39,7 @@ s = TStopping(nlp, rtol=0.0, max_eval = 500000, max_iter = 100000, max_time = 1.
 
 
 reset!(nlp)
-s = TStopping(nlp, rtol=0.0, max_eval = 500000, max_iter = 100000, max_time = 2.0)
+s = TStopping(rtol=0.0, max_eval = 500000, max_iter = 100000, max_time = 2.0)
 (x, f, gNorm, iter, optimal, tired, status, Stime) = steepest(nlp, s=s, verbose=false)
 @printf("%-5s  %3d  %9.2e  %7.1e  %5d  %5d  %6d   %6d  %-20s  %7.3e\n",
         nlp.meta.name, nlp.meta.nvar, f, gNorm,
@@ -50,7 +50,7 @@ s = TStopping(nlp, rtol=0.0, max_eval = 500000, max_iter = 100000, max_time = 2.
 
 
 reset!(nlp)
-s = TStopping(nlp)
+s = TStopping()
 (x, f, gNorm, iter, optimal, tired, status, Stime) = steepest(nlp, s=s, verbose=false, bk_max=2)
 @printf("%-5s  %3d  %9.2e  %7.1e  %5d  %5d  %6d   %6d  %-20s  %7.3e\n",
         nlp.meta.name, nlp.meta.nvar, f, gNorm,
--- test/steepestS.jl
+++ test/steepestS.jl
@@ -1,6 +1,6 @@
-export steepestS
+export steepest
 
-function steepestS(nlp :: AbstractNLPModel;
+function steepest(nlp :: AbstractNLPModel;
                   s :: TStopping = TStopping(),
                   verbose :: Bool=true,
                   linesearch :: Function = Newarmijo_wolfe,
