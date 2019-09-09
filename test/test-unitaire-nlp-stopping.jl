423eb9e59a8f58cc50d08c42f0527582b618a003
tmigot <tangi.migot@gmail.com>
2019-09-10 01:06:04 +0200

    add_stop! move

--- test/test-unitaire-nlp-stopping.jl
+++ test/test-unitaire-nlp-stopping.jl
@@ -18,3 +18,5 @@ fill_in!(stop_nlp, a)
 
 # we make sure the optimality check works properly
 @test stop!(stop_nlp)
+# we make sure the counter of stop works properly
+@test stop_nlp.meta.nb_of_stop == 1
