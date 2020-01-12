111381ac2cb8fb623a4ebb63f57dfc7c5a650451
tmigot <tangi.migot@gmail.com>
2020-01-12 20:09:10 +0100

    rosenbrock in a separate file

--- test/test-unitaire-generic-stopping.jl
+++ test/test-unitaire-generic-stopping.jl
@@ -1,21 +1,4 @@
-function rosenbrock(x)
-
-    n = 6;
-
-    # Initializations
-    f = 0
-
-    evenIdx = 2:2:n
-    oddIdx  = 1:2:(n-1)
-
-    f1  = x[evenIdx] .- x[oddIdx].^2
-    f2  = 1 .- x[oddIdx]
-
-    # Function
-    f   = sum( f1.^2 .+ f2.^2 )
-
-    return f
-end
+include("rosenbrock.jl")
 
 #We build a first stopping: to test the change of tol_check function
 x0 = ones(6)
