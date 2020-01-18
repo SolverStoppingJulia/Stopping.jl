87cec9d906dfc1f7bc58d337d7fdb07cb0954d17
tmigot <tangi.migot@gmail.com>
2020-01-18 01:58:23 +0100

    correct KKT function

--- src/Stopping/nlp_admissible_functions.jl
+++ src/Stopping/nlp_admissible_functions.jl
@@ -24,29 +24,46 @@ length(lambda) > 0
 """
 function _grad_lagrangian(pb    :: AbstractNLPModel,
                           state :: NLPAtX)
- return state.gx + state.mu + state.Jx' * state.lambda
+ if pb.meta.ncon == 0 & !has_bounds(pb)
+  return state.gx
+ elseif pb.meta.ncon == 0
+  return state.gx + state.mu
+ else
+  return state.gx + state.mu + state.Jx' * state.lambda
+ end
 end
 
 function _sign_multipliers_bounds(pb    :: AbstractNLPModel,
                                   state :: NLPAtX)
- return vcat(min.(max.( state.mu,0), - state.x + pb.meta.uvar),
-             min.(max.(-state.mu,0),   state.x - pb.meta.lvar))
+ if has_bounds(pb)
+  return vcat(min.(max.( state.mu,0.0), - state.x + pb.meta.uvar),
+              min.(max.(-state.mu,0.0),   state.x - pb.meta.lvar))
+ else
+  return zeros(0)
+ end
 end
 
 function _sign_multipliers_nonlin(pb    :: AbstractNLPModel,
                                   state :: NLPAtX)
-
- return vcat(min.(max.( state.lambda,0), - state.cx + pb.meta.ucon),
-             min.(max.(-state.lambda,0),   state.cx - pb.meta.lcon))
+ if pb.meta.ncon == 0
+  return zeros(0)
+ else
+  return vcat(min.(max.( state.lambda,0.0), - state.cx + pb.meta.ucon),
+              min.(max.(-state.lambda,0.0),   state.cx - pb.meta.lcon))
+ end
 end
 
 function _feasibility(pb    :: AbstractNLPModel,
                       state :: NLPAtX)
-
- return vcat(max.(  state.cx - pb.meta.ucon,0),
-             max.(- state.cx + pb.meta.lcon,0),
-             max.(  state.x  - pb.meta.uvar,0),
-             max.(- state.x  + pb.meta.lvar,0))
+ if pb.meta.ncon == 0
+  return vcat(max.(  state.x  - pb.meta.uvar,0.0),
+              max.(- state.x  + pb.meta.lvar,0.0))
+ else
+  return vcat(max.(  state.cx - pb.meta.ucon,0.0),
+              max.(- state.cx + pb.meta.lcon,0.0),
+              max.(  state.x  - pb.meta.uvar,0.0),
+              max.(- state.x  + pb.meta.lvar,0.0))
+ end
 end
 
 """
