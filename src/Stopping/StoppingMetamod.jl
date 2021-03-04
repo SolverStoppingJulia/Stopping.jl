5a416fe0fe070085a75a4c421168a0de19cad50f
tmigot <tangi.migot@gmail.com>
2021-03-05 00:57:02 +0100

    alternative tol_check

--- src/Stopping/StoppingMetamod.jl
+++ src/Stopping/StoppingMetamod.jl
@@ -4,58 +4,62 @@ Type: StoppingMeta
 Methods: no methods.
 
 Attributes:
-- atol : absolute tolerance.
-- rtol : relative tolerance.
-- optimality0 : optimality score at the initial guess.
-- tol_check : Function of *atol*, *rtol* and *optimality0* testing a score to zero.
-- tol_check_neg : Function of *atol*, *rtol* and *optimality0* testing a score to zero.
-- check_pos : pre-allocation for positive tolerance
-- check_neg : pre-allocation for negative tolerance
-- recomp_tol : true if tolerances are updated
-- optimality_check : a stopping criterion via an admissibility function
-- unbounded_threshold : threshold for unboundedness of the problem.
-- unbounded_x : threshold for unboundedness of the iterate.
-- max_f :  maximum number of function (and derivatives) evaluations.
-- max_cntrs  : Dict contains the maximum number of evaluations
-- max_eval :  maximum number of function (and derivatives) evaluations.
-- max_iter : threshold on the number of stop! call/number of iteration.
-- max_time : time limit to let the algorithm run.
-- nb\\_of\\_stop : keep track of the number of stop! call/iteration.
-- start_time : keep track of the time at the beginning.
-- fail\\_sub\\_pb : status.
-- unbounded : status.
-- unbounded_pb : status.
-- tired : status.
-- stalled : status.
-- iteration_limit : status.
-- resources : status.
-- optimal : status.
-- infeasible : status.
-- main_pb : status.
-- domainerror : status.
-- suboptimal : status.
-- stopbyuser : status.
-- exception : status.
+- atol: absolute tolerance.
+- rtol: relative tolerance.
+- optimality0: optimality score at the initial guess.
+- tol_check: Function of *atol*, *rtol* and *optimality0* testing a score to zero.
+- tol_check_neg: Function of *atol*, *rtol* and *optimality0* testing a score to zero.
+- check_pos: pre-allocation for positive tolerance
+- check_neg: pre-allocation for negative tolerance
+- recomp_tol: true if tolerances are updated
+- optimality_check: a stopping criterion via an admissibility function
+- unbounded_threshold: threshold for unboundedness of the problem.
+- unbounded_x: threshold for unboundedness of the iterate.
+- max_f: maximum number of function (and derivatives) evaluations.
+- max_cntrs: Dict contains the maximum number of evaluations
+- max_eval:  maximum number of function (and derivatives) evaluations.
+- max_iter: threshold on the number of stop! call/number of iteration.
+- max_time: time limit to let the algorithm run.
+- nb\\_of\\_stop: keep track of the number of stop! call/iteration.
+- start_time: keep track of the time at the beginning.
+- fail\\_sub\\_pb: status.
+- unbounded: status.
+- unbounded_pb: status.
+- tired: status.
+- stalled: status.
+- iteration_limit: status.
+- resources: status.
+- optimal: status.
+- infeasible: status.
+- main_pb: status.
+- domainerror: status.
+- suboptimal: status.
+- stopbyuser: status.
+- exception: status.
 
-- meta_user_struct :  Any
-- user_check_func! : Function (AbstractStopping, Bool) -> callback.
+- meta_user_struct:  Any
+- user_check_func!: Function (AbstractStopping, Bool) -> callback.
 
-`StoppingMeta(;atol :: Number = 1.0e-6, rtol :: Number = 1.0e-15, optimality0 :: Number = 1.0, tol_check :: Function = (atol,rtol,opt0) -> max(atol,rtol*opt0), unbounded_threshold :: Number = 1.0e50, unbounded_x :: Number = 1.0e50, max_f :: Int = typemax(Int), max_eval :: Int = 20000, max_iter :: Int = 5000, max_time :: Number = 300.0, start_time :: Float64 = NaN, meta_user_struct :: Any = nothing, kwargs...)`
+`StoppingMeta(;atol :: Number = 1.0e-6, rtol :: Number = 1.0e-15, optimality0 :: Number = 1.0, tol_check :: Function = (atol,rtol,opt0) -> max(atol,rtol*opt0), tol_check_neg :: Function = (atol,rtol,opt0) -> -max(atol,rtol*opt0), unbounded_threshold :: Number = 1.0e50, unbounded_x :: Number = 1.0e50, max_f :: Int = typemax(Int), max_eval :: Int = 20000, max_iter :: Int = 5000, max_time :: Number = 300.0, start_time :: Float64 = NaN, meta_user_struct :: Any = nothing, kwargs...)`
+
+an alternative with constant tolerances:
+
+`StoppingMeta(tol_check :: T, tol_check_neg :: T;atol :: Number = 1.0e-6, rtol :: Number = 1.0e-15, optimality0 :: Number = 1.0, unbounded_threshold :: Number = 1.0e50, unbounded_x :: Number = 1.0e50, max_f :: Int = typemax(Int), max_eval :: Int = 20000, max_iter :: Int = 5000, max_time :: Number = 300.0, start_time :: Float64 = NaN, meta_user_struct :: Any = nothing, kwargs...)`
 
 Note:
-- It is a mutable struct, therefore we can modify elements of a *StoppingMeta*.
-- The *nb\\_of\\_stop* is incremented everytime *stop!* or *update\\_and\\_stop!* is called
-- The *optimality0* is modified once at the beginning of the algorithm (*start!*)
-- The *start_time* is modified once at the beginning of the algorithm (*start!*)
+- It is a mutable struct, therefore we can modify elements of a `StoppingMeta`.
+- The `nb_of_stop` is incremented everytime `stop!` or `update_and_stop!` is called
+- The `optimality0` is modified once at the beginning of the algorithm (`start!`)
+- The `start_time` is modified once at the beginning of the algorithm (`start!`)
       if not precised before.
-- The different status: *fail\\_sub\\_pb*, *unbounded*, *unbounded_pb*, *tired*, *stalled*,
-      *iteration_limit*, *resources*, *optimal*, *main_pb*, *domainerror*, *suboptimal*, *infeasible*
-- *fail\\_sub\\_pb*, *suboptimal*, and *infeasible* are modified by the algorithm.
-- *optimality_check* takes two inputs (*AbstractNLPModel*, *NLPAtX*)
- and returns a *Number* or an *AbstractVector* to be compared to *0*.
-- *optimality_check* does not necessarily fill in the State.
+- The different status: `fail_sub_pb`, `unbounded`, `unbounded_pb`, `tired`, `stalled`,
+      `iteration_limit`, `resources`, `optimal`, `main_pb`, `domainerror`, `suboptimal`, `infeasible`
+- `fail_sub_pb`, `suboptimal`, and `infeasible` are modified by the algorithm.
+- `optimality_check` takes two inputs (`AbstractNLPModel`, `NLPAtX`)
+ and returns a `Number` or an `AbstractVector` to be compared to `0`.
+- `optimality_check` does not necessarily fill in the State.
 
-Examples: `StoppingMeta()`
+Examples: `StoppingMeta()`, `StoppingMeta(1., -1.)`
 """
 mutable struct StoppingMeta{TolType <: Number, 
                             CheckType, #Type of the tol_check output
@@ -67,10 +71,10 @@ mutable struct StoppingMeta{TolType <: Number,
   atol                :: TolType # absolute tolerance
   rtol                :: TolType # relative tolerance
   optimality0         :: TolType # value of the optimality residual at starting point
-  tol_check           :: Function #function of atol, rtol and optimality0
+  tol_check           :: Union{Function, CheckType} #function of atol, rtol and optimality0
                                   #by default: tol_check = max(atol, rtol * optimality0)
                                   #other example: atol + rtol * optimality0
-  tol_check_neg       :: Function # function of atol, rtol and optimality0
+  tol_check_neg       :: Union{Function, CheckType} # function of atol, rtol and optimality0
   check_pos           :: CheckType #pre-allocation for positive tolerance
   check_neg           :: CheckType #pre-allocation for negative tolerance
   optimality_check    :: Function # stopping criterion
@@ -114,6 +118,63 @@ mutable struct StoppingMeta{TolType <: Number,
   meta_user_struct    :: MUS
   user_check_func!    :: Function #called dans Stopping._user_check!(stp, x)
 
+end
+
+function StoppingMeta(tol_check           :: CheckType,
+                      tol_check_neg       :: CheckType;
+                      atol                :: Number   = 1.0e-6,
+                      rtol                :: Number   = 1.0e-15,
+                      optimality0         :: Number   = 1.0,
+                      optimality_check    :: Function = (a,b) -> Inf,
+                      recomp_tol          :: Bool     = false,
+                      unbounded_threshold :: Number   = 1.0e50, #typemax(Float64)
+                      unbounded_x         :: Number   = 1.0e50,
+                      max_f               :: Int      = typemax(Int),
+                      max_cntrs           :: Dict{Symbol,Int} = Dict{Symbol,Int64}(),
+                      max_eval            :: Int      = 20000,
+                      max_iter            :: Int      = 5000,
+                      max_time            :: Float64  = 300.0,
+                      start_time          :: Float64  = NaN,
+                      meta_user_struct    :: Any      = nothing,
+                      user_check_func!    :: Function = (stp :: AbstractStopping, start :: Bool) -> nothing,
+                      kwargs...) where CheckType
+
+  check_pos = tol_check
+  check_neg = tol_check_neg
+
+  # This might be an expansive step.
+  # if (true in (check_pos .< check_neg)) #any(x -> x, check_pos .< check_neg)
+  #     throw(ErrorException("StoppingMeta: tol_check should be greater than tol_check_neg."))
+  # end
+
+  fail_sub_pb     = false
+  unbounded       = false
+  unbounded_pb    = false
+  tired           = false
+  stalled         = false
+  iteration_limit = false
+  resources       = false
+  optimal         = false
+  infeasible      = false
+  main_pb         = false
+  domainerror     = false
+  suboptimal      = false
+  stopbyuser      = false
+  exception       = false
+
+  nb_of_stop = 0
+
+  #new{TolType, typeof(check_pos), typeof(meta_user_struct)}
+  return StoppingMeta(atol, rtol, optimality0,
+                      tol_check, tol_check_neg,
+                      check_pos, check_neg, optimality_check, recomp_tol,
+                      unbounded_threshold, unbounded_x,
+                      max_f, max_cntrs, max_eval, max_iter, 
+                      max_time, nb_of_stop, start_time,
+                      fail_sub_pb, unbounded, unbounded_pb, tired, stalled,
+                      iteration_limit, resources, optimal, infeasible, main_pb,
+                      domainerror, suboptimal, stopbyuser, exception,
+                      meta_user_struct, user_check_func!)
 end
 
 function StoppingMeta(;atol               :: Number   = 1.0e-6,
--- test/test-stopping/unit-test-stopping-meta.jl
+++ test/test-stopping/unit-test-stopping-meta.jl
@@ -32,7 +32,8 @@
     @test test_meta.exception             == false
     @test test_meta.infeasible            == false
     @test test_meta.nb_of_stop            == 0
-    @test test_meta.meta_user_struct      == nothing
+    @test isnothing(test_meta.meta_user_struct)
+    @test test_meta.recomp_tol
 
     @test_throws MethodError StoppingMeta(tol_check = x -> x)
     @test_throws MethodError StoppingMeta(tol_check_neg = x -> x)
@@ -60,4 +61,41 @@
     @test !OK_check(test_meta)
     
     #@test_throws ErrorException("StoppingMeta: tol_check should be greater than tol_check_neg.") StoppingMeta(tol_check_neg = (a,b,c) -> Inf)
+end
+
+@testset "StoppingMeta - 2nd constructor" begin
+    #Checked the default constructor
+    test_meta = StoppingMeta(ones(2), -ones(2))
+        
+    @test Stopping.checktype(test_meta) == Array{Float64,1}
+    @test Stopping.toltype(test_meta)   == Float64
+    @test Stopping.metausertype(test_meta) == Nothing
+    @test Stopping.inttype(test_meta) == Int64
+
+    @test test_meta.optimality0           == 1.0
+    @test test_meta.optimality_check(1,1) == Inf
+    @test test_meta.unbounded_threshold   == 1.0e50
+    @test test_meta.unbounded_x           == 1.0e50
+    @test test_meta.max_f                 == 9223372036854775807
+    @test test_meta.max_cntrs             == Dict()
+    @test test_meta.max_eval              == 20_000
+    @test test_meta.max_iter              == 5_000
+    @test test_meta.max_time              == 300.0
+    @test isnan(test_meta.start_time)
+    @test test_meta.fail_sub_pb           == false
+    @test test_meta.unbounded             == false
+    @test test_meta.unbounded_pb          == false
+    @test test_meta.tired                 == false
+    @test test_meta.stalled               == false
+    @test test_meta.iteration_limit       == false
+    @test test_meta.optimal               == false
+    @test test_meta.suboptimal            == false
+    @test test_meta.main_pb               == false
+    @test test_meta.stopbyuser            == false
+    @test test_meta.exception             == false
+    @test test_meta.infeasible            == false
+    @test test_meta.nb_of_stop            == 0
+    @test isnothing(test_meta.meta_user_struct)
+    @test !test_meta.recomp_tol
+
 end
