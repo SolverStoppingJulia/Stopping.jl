7e094f44847c115686c5ab9737d966b9f5b021cd
tmigot <tangi.migot@gmail.com>
2021-12-01 12:13:40 -0500

    update stopping constructors docs

--- src/Stopping/GenericStoppingmod.jl
+++ src/Stopping/GenericStoppingmod.jl
@@ -1,40 +1,51 @@
 """
- Type: GenericStopping
+ Type: `GenericStopping`
 
- Methods: start!, stop!, update\\_and\\_start!, update\\_and\\_stop!, fill\\_in!, reinit!, status
+ Methods: `start!`, `stop!`, `update_and_start!`, `update_and_stop!`, `fill_in!`, `reinit!`, `status`
 
  A generic Stopping to solve instances with respect to some
  optimality conditions. Optimality is decided by computing a score, which is then
  tested to zero.
 
  Tracked data include:
-- pb         : A problem
-- state      : The information relative to the problem, see `GenericState`.
-- (opt) meta : Metadata relative to a stopping criterion, see `StoppingMeta`.
-- (opt) main_stp : Stopping of the main loop in case we consider a Stopping
+- `pb`         : A problem
+- `current_state` : The information relative to the problem, see `GenericState`.
+- (opt) `meta` : Metadata relative to a stopping criteria, see `StoppingMeta`.
+- (opt) `main_stp` : Stopping of the main loop in case we consider a Stopping
                        of a subproblem.
-                       If not a subproblem, then `nothing`.
-- (opt) listofstates : ListofStates designed to store the history of States.
-- (opt) stopping_user_struct : Contains any structure designed by the user.
+                       If not a subproblem, then `VoidStopping`.
+- (opt) `listofstates` : `ListofStates` designed to store the history of States.
+- (opt) `stopping_user_struct` : Contains a structure designed by the user.
 
- Constructor: `GenericStopping(:: Any, :: AbstractState; meta :: AbstractStoppingMeta = StoppingMeta(), main_stp :: Union{AbstractStopping, Nothing} = nothing, stopping_user_struct :: Any = nothing, kwargs...)`
+ Constructors: 
+- `GenericStopping(pb, meta::AbstractStoppingMeta, stop_remote::AbstractStopRemoteControl, state::AbstractState; main_stp::AbstractStopping=VoidStopping(), list::AbstractListofStates = VoidListofStates(), user_struct::AbstractDict = Dict(), kwargs...)`
+     The default constructor.
+- `GenericStopping(pb, meta::AbstractStoppingMeta, state::AbstractState; main_stp::AbstractStopping=VoidStopping(), list::AbstractListofStates = VoidListofStates(), user_struct::AbstractDict = Dict(), kwargs...)`
+     The one passing the `kwargs` to the `stop_remote`.
+- `GenericStopping(pb, state::AbstractState; stop_remote::AbstractStopRemoteControl = StopRemoteControl(), main_stp::AbstractStopping=VoidStopping(), list::AbstractListofStates = VoidListofStates(), user_struct::AbstractDict = Dict(), kwargs...)`
+     The one passing the `kwargs` to the `meta`.
+- `GenericStopping(pb, stop_remote::AbstractStopRemoteControl, state::AbstractState; main_stp::AbstractStopping=VoidStopping(), list::AbstractListofStates = VoidListofStates(), user_struct::AbstractDict = Dict(), kwargs...)`
+     The one passing the `kwargs` to the `meta`.
+- `GenericStopping(pb, x; n_listofstates=, kwargs...)`
+     The one setting up a default state using x, and initializing the list of states if `n_listofstates>0`. 
+
 
  Note: Metadata can be provided by the user or created with the Stopping
        constructor via kwargs. If a specific StoppingMeta is given and
        kwargs are provided, the kwargs have priority.
 
  Examples:
- GenericStopping(pb, GenericState(ones(2)), rtol = 1e-1)
+ `GenericStopping(pb, GenericState(ones(2)), rtol = 1e-1)`
 
  Besides optimality conditions, we consider classical emergency exit:
-       - domain error        (for instance: NaN in x)
-       - unbounded problem   (not implemented)
-       - unbounded x         (x is too large)
-       - tired problem       (time limit attained)
-       - resources exhausted (not implemented)
-       - stalled problem     (not implemented)
-       - iteration limit     (maximum number of iteration (i.e. nb of stop) attained)
-       - main_pb limit       (tired or resources of main problem exhausted)
+- domain error        (for instance: NaN in x)
+- unbounded problem   (not implemented)
+- unbounded x         (x is too large)
+- tired problem       (time limit attained)
+- resources exhausted (not implemented)
+- stalled problem     (not implemented)
+- iteration limit     (maximum number of iteration (i.e. nb of stop) attained)
+- main_pb limit       (tired or resources of main problem exhausted)
 
  There is an additional default constructor which creates a Stopping with a default State.
 
@@ -43,7 +54,7 @@
  Note: Keywords arguments are forwarded to the classical constructor.
 
  Examples:
- GenericStopping(pb, x0, rtol = 1e-1)
+ `GenericStopping(pb, x0, rtol = 1e-1)`
 """
 mutable struct GenericStopping{Pb, M, SRC, T, MStp, LoS} <:
                AbstractStopping{Pb, M, SRC, T, MStp, LoS}
--- src/Stopping/LinearAlgebraStopping.jl
+++ src/Stopping/LinearAlgebraStopping.jl
@@ -1,8 +1,8 @@
 """
 Type: LAStopping
 
-Methods: start!, stop!, update\\_and\\_start!, update\\_and\\_stop!, fill_in!, reinit!, status
-linear\\_system\\_check, normal\\_equation\\_check
+Methods: `start!`, `stop!`, `update_and_start!`, `update_and_stop!`, `fill_in!`, `reinit!`, `status`, 
+`linear_system_check`, `normal_equation_check`
 
 Specialization of GenericStopping. Stopping structure for linear algebra
 solving either
@@ -11,36 +11,41 @@ solving either
 
 or
 
-``min\\_{x} \\tfrac{1}{2}\\|Ax - b\\|^2``.
+```math
+min\\_{x} \\tfrac{1}{2}\\|Ax - b\\|^2
+```
 
 Attributes:
-- pb         : a problem using LLSModel (designed for linear least square problem, see https://github.com/JuliaSmoothOptimizers/NLPModels.jl/blob/master/src/lls_model.jl )
-- state      : The information relative to the problem, see GenericState
-- (opt) meta : Metadata relative to stopping criterion, see `StoppingMeta`.
-- (opt) main_stp : Stopping of the main loop in case we consider a Stopping
+- `pb`         : a problem using, for instance, either `LLSModel` (designed for linear least square problem, see https://github.com/JuliaSmoothOptimizers/LLSModels.jl ) or `LinearSystem`.
+- `current_state`      : The information relative to the problem, see `GenericState`.
+- (opt) `meta` : Metadata relative to stopping criteria, see `StoppingMeta`.
+- (opt) `main_stp` : Stopping of the main loop in case we consider a Stopping
                           of a subproblem.
-                          If not a subproblem, then nothing.
-- (opt) listofstates : ListofStates designed to store the history of States.
-- (opt) stopping_user_struct : Contains any structure designed by the user.
+                          If not a subproblem, then `VoidStopping`.
+- (opt) `listofstates` : ListofStates designed to store the history of States.
+- (opt) `stopping_user_struct` : Contains a structure designed by the user.
 
-`LAStopping(:: LLSModel, :: AbstractState; meta :: AbstractStoppingMeta = StoppingMeta() main_stp :: Union{AbstractStopping, Nothing} = nothing, stopping_user_struct :: Any = nothing, kwargs...)`
+Constructors: 
+- `LAStopping(pb, meta::AbstractStoppingMeta, stop_remote::AbstractStopRemoteControl, state::AbstractState; main_stp::AbstractStopping=VoidStopping(), list::AbstractListofStates = VoidListofStates(), user_struct::AbstractDict = Dict(), zero_start::Bool = false)`
+     The default constructor.
+- `LAStopping(pb, meta::AbstractStoppingMeta, state::AbstractState; main_stp::AbstractStopping=VoidStopping(), list::AbstractListofStates = VoidListofStates(), user_struct::AbstractDict = Dict(), zero_start::Bool = false, kwargs...)`
+     The one passing the `kwargs` to the `stop_remote`.
+- `LAStopping(pb, state::AbstractState; stop_remote::AbstractStopRemoteControl = StopRemoteControl(), main_stp::AbstractStopping=VoidStopping(), list::AbstractListofStates = VoidListofStates(), user_struct::AbstractDict = Dict(), zero_start::Bool = false, kwargs...)`
+     The one passing the `kwargs` to the `meta`.
+- `LAStopping(:: Union{AbstractLinearOperator, AbstractMatrix}, :: AbstractVector; sparse::Bool = true, n_listofstates::Int = 0, kwargs...)`
+     The one setting up a default problem (`sparse ? LLSModel(A, b) : LinearSystem(A, b)`), a default `GenericState` using x, and initializing the list of states if `n_listofstates>0`. 
+- `LAStopping(:: Union{AbstractLinearOperator, AbstractMatrix}, :: AbstractVector, :: AbstractState; sparse::Bool = true, kwargs...)`
+     The one setting up a default problem (`sparse ? LLSModel(A, b) : LinearSystem(A, b)`). 
 
-Note:
-- Kwargs are forwarded to the classical constructor.
-- Not specific State targeted
+Notes:
+- No specific State targeted
 - State don't necessarily keep track of evals
-- Evals are checked only for pb.A being a LinearOperator
-- zero_start is true if 0 is the initial guess (not check automatically)
-- LLSModel counter follow NLSCounters (see init_max_counters_NLS in NLPStoppingmod.jl)
-- By default, meta.max\\_cntrs is initialized with an NLSCounters
+- Evals are checked only for `pb.A` being a LinearOperator
+- `zero_start` is true if 0 is the initial guess (not check automatically)
+- `LLSModel` counter follow `NLSCounters` (see `init_max_counters_NLS` in `NLPStoppingmod.jl`)
+- By default, `meta.max_cntrs` is initialized with an NLSCounters
 
-There is additional constructors:
-
-`LAStopping(:: Union{AbstractLinearOperator, AbstractMatrix}, :: AbstractVector, kwargs...)`
-
-`LAStopping(:: Union{AbstractLinearOperator, AbstractMatrix}, :: AbstractVector, :: AbstractState, kwargs...)`
-
-See also GenericStopping, NLPStopping, LS\\_Stopping, linear\\_system\\_check, normal\\_equation\\_check
+See also `GenericStopping`, `NLPStopping`, `linear_system_check`, `normal_equation_check`
  """
 mutable struct LAStopping{Pb, M, SRC, T, MStp, LoS} <: AbstractStopping{Pb, M, SRC, T, MStp, LoS}
 
