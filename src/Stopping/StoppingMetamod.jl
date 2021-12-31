18cd0c011f60ed1e0b242f254fe2fc17635da34d
tmigot <tangi.migot@gmail.com>
2021-12-31 17:19:22 -0500

    set default optimality_check to 1

--- src/Stopping/StoppingMetamod.jl
+++ src/Stopping/StoppingMetamod.jl
@@ -4,41 +4,40 @@ Type: StoppingMeta
 Methods: no methods.
 
 Attributes:
-- atol: absolute tolerance.
-- rtol: relative tolerance.
-- optimality0: optimality score at the initial guess.
-- tol_check: Function of `atol`, `rtol` and `optimality0` testing a score to zero.
-- tol_check_neg: Function of `atol`, `rtol` and `optimality0` testing a score to zero.
-- check_pos: pre-allocation for positive tolerance
-- check_neg: pre-allocation for negative tolerance
-- recomp_tol: true if tolerances are updated
-- optimality_check: a stopping criterion via an admissibility function
-- unbounded_threshold: threshold for unboundedness of the problem.
-- unbounded_x: threshold for unboundedness of the iterate.
-- max_f: maximum number of function (and derivatives) evaluations.
-- max_cntrs: Dict contains the maximum number of evaluations
-- max_eval:  maximum number of function (and derivatives) evaluations.
-- max_iter: threshold on the number of stop! call/number of iteration.
-- max_time: time limit to let the algorithm run.
-- nb\\_of\\_stop: keep track of the number of stop! call/iteration.
-- start_time: keep track of the time at the beginning.
-- fail\\_sub\\_pb: status.
-- unbounded: status.
-- unbounded_pb: status.
-- tired: status.
-- stalled: status.
-- iteration_limit: status.
-- resources: status.
-- optimal: status.
-- infeasible: status.
-- main_pb: status.
-- domainerror: status.
-- suboptimal: status.
-- stopbyuser: status.
-- exception: status.
-
-- meta_user_struct:  Any
-- user_check_func!: Function (AbstractStopping, Bool) -> callback.
+- `atol`: absolute tolerance.
+- `rtol`: relative tolerance.
+- `optimality0`: optimality score at the initial guess.
+- `tol_check`: Function of `atol`, `rtol` and `optimality0` testing a score to zero.
+- `tol_check_neg`: Function of `atol`, `rtol` and `optimality0` testing a score to zero.
+- `check_pos`: pre-allocation for positive tolerance
+- `check_neg`: pre-allocation for negative tolerance
+- `recomp_tol`: true if tolerances are updated
+- `optimality_check`: a stopping criterion via an admissibility function
+- `unbounded_threshold`: threshold for unboundedness of the problem.
+- `unbounded_x`: threshold for unboundedness of the iterate.
+- `max_f`: maximum number of function (and derivatives) evaluations.
+- `max_cntrs`: Dict contains the maximum number of evaluations
+- `max_eval`:  maximum number of function (and derivatives) evaluations.
+- `max_iter`: threshold on the number of stop! call/number of iteration.
+- `max_time`: time limit to let the algorithm run.
+- `nb_of_stop`: keep track of the number of stop! call/iteration.
+- `start_time`: keep track of the time at the beginning.
+- `fail_sub_pb`: status.
+- `unbounded`: status.
+- `unbounded_pb`: status.
+- `tired`: status.
+- `stalled`: status.
+- `iteration_limit`: status.
+- `resources`: status.
+- `optimal`: status.
+- `infeasible`: status.
+- `main_pb`: status.
+- `domainerror`: status.
+- `suboptimal`: status.
+- `stopbyuser`: status.
+- `exception`: status.
+- `meta_user_struct`:  Any
+- `user_check_func!`: Function (AbstractStopping, Bool) -> callback.
 
 `StoppingMeta(;atol :: Number = 1.0e-6, rtol :: Number = 1.0e-15, optimality0 :: Number = 1.0, tol_check :: Function = (atol,rtol,opt0) -> max(atol,rtol*opt0), tol_check_neg :: Function = (atol,rtol,opt0) -> -max(atol,rtol*opt0), unbounded_threshold :: Number = 1.0e50, unbounded_x :: Number = 1.0e50, max_f :: Int = typemax(Int), max_eval :: Int = 20000, max_iter :: Int = 5000, max_time :: Number = 300.0, start_time :: Float64 = NaN, meta_user_struct :: Any = nothing, kwargs...)`
 
@@ -126,7 +125,7 @@ function StoppingMeta(
   atol::Number = 1.0e-6,
   rtol::Number = 1.0e-15,
   optimality0::Number = 1.0,
-  optimality_check::Function = (a, b) -> Inf,
+  optimality_check::Function = (a, b) -> 1.0,
   recomp_tol::Bool = false,
   unbounded_threshold::Number = 1.0e50, #typemax(Float64)
   unbounded_x::Number = 1.0e50,
@@ -211,7 +210,7 @@ function StoppingMeta(;
   tol_check::Function = (atol::Number, rtol::Number, opt0::Number) -> max(atol, rtol * opt0),
   tol_check_neg::Function = (atol::Number, rtol::Number, opt0::Number) ->
     -tol_check(atol, rtol, opt0),
-  optimality_check::Function = (a, b) -> Inf,
+  optimality_check::Function = (a, b) -> 1.0,
   recomp_tol::Bool = true,
   unbounded_threshold::Number = 1.0e50, #typemax(Float64)
   unbounded_x::Number = 1.0e50,
