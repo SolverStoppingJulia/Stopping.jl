1b87a75105f25d0a21e9ec9219acf375819fcd7e
tmigot <tangi.migot@gmail.com>
2022-08-18 18:32:03 -0400

    Use `pinv` instead of `\` in multiplier computation

--- src/Stopping/nlp_compute_multiplier.jl
+++ src/Stopping/nlp_compute_multiplier.jl
@@ -33,7 +33,7 @@ function _compute_mutliplier(
 
   mu, lambda = zeros(eltype(T), n), zeros(eltype(T), nc)
   if (Ib != []) || (Ic != [])
-    l = Jc \ (-gx)
+    l = pinv(Jc) * (-gx)
     mu[Ib], lambda[Ic] = l[1:length(Ib)], l[(length(Ib) + 1):length(l)]
   end
 
